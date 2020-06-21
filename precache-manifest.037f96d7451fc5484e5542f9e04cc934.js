41d743de4d31c7d67161e3112e8aadf7d8acf26a
devi <devidnr4@gmail.com>
2020-06-21 19:58:23 +0530

    Updates

---
+++ precache-manifest.037f96d7451fc5484e5542f9e04cc934.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed1a57709da7d594ff8e513d31e21ba8",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "dc79e41bf27f59490002",
+    "url": "/react-app/static/css/main.ab003eb6.chunk.css"
+  },
+  {
+    "revision": "7ea7fa3654f44c214f5f",
+    "url": "/react-app/static/js/2.80fda868.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.80fda868.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc79e41bf27f59490002",
+    "url": "/react-app/static/js/main.9eabfee1.chunk.js"
+  },
+  {
+    "revision": "e2c6b0a16d423a796dce",
+    "url": "/react-app/static/js/runtime-main.9a102076.js"
+  },
+  {
+    "revision": "4948508ccfd3552d72bc0783dfe5502f",
+    "url": "/react-app/static/media/delivery-man.4948508c.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app/precache-manifest.f225daedbdeb6752487c58b53e621b38.js"
+  "/react-app/precache-manifest.037f96d7451fc5484e5542f9e04cc934.js"
 );
 
 self.addEventListener('message', (event) => {
